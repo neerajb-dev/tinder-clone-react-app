fc2ab49c9d18f706da8b1f238c8e8cc7d1f1dd17
Neeraj Bhardwaj <neerajb@yahoo.com>
2022-07-27 19:35:34 +0530

    SwipeButtons component done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from './Header';
 import TinderCards from './TinderCards';
+import SwipeButton from './SwipeButton';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <TinderCards />
-      {/* SwipeButtons */}
+      <SwipeButton />
     </div>
   );
 }
