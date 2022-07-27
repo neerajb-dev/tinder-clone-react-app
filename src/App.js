226d98d9b167582ea363e6c9b2343fe0e96a1426
Neeraj Bhardwaj <neerajb@yahoo.com>
2022-07-27 18:38:33 +0530

    header component done

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Header from './Header';
 function App() {
   return (
     <div className="App">
-      <h1>Lets build the MERN TINDER clone guys!!!</h1>
       <Header />
-      {/* Header */}
       {/* TinderCards */}
       {/* SwipeButtons */}
     </div>
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,27 @@
 import React from "react";
 import "./Header.css";
+import PersonIcon from "@mui/icons-material/Person";
+import IconButton from "@mui/material/IconButton";
+import ForumIcon from "@mui/icons-material/Forum";
 
 function Header() {
     return (
         <div className="header">
-            <h2>I am the header</h2>
+            {/* person icon */}
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icon" />
+            </IconButton>
+
+            {/* tinder logo */}
+            <img
+                className="header__logo"
+                src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-icon.png"
+                alt="tinder-logo"
+            />
+
+            <IconButton>
+                <ForumIcon fontSize="large" className="header__icon" />
+            </IconButton>
         </div>
     )
 }
